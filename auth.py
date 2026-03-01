ed2f97f5da948e086d7ebb5c6846f133320e78cf
maleyalui <maleyalui1@gmail.com>
2026-03-01 19:48:01 +0300

    SCRUM-7 Handle decorators & authorisation (i.e. Decorators.py & auth.py)

--- auth.py
+++ auth.py
@@ -1,7 +1,10 @@
 # calls the register user and login user from user_services
 
 #Import Dependancies
-
+from services.users_services import register_user, login_user
 #Register
-
+def register_user(id_no, name, password, role="customer"):
+    return register_user(id_no, name, password, role)
 #Login
+def login_user(id_no, password):
+    return login_user(id_no, password)
